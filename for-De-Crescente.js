721f56bbe95c5cb6a03eafa7754c933e0cc55b55
iohanjheremy <iohanjheremy11@gmail.com>
2023-02-19 11:20:08 -0400

    atvds do caderno

---
+++ for-De-Crescente.js
@@ -0,0 +1,8 @@
+/*for(let i = 100; i >= 1; i--){
+    console.log(i)
+}*/ //DECRESECENTE 100 -> 1
+
+for(let i = 1; i <= 100; i++){
+    console.log(i)
+}
+//CRESCENTE 1 -> 100
---
+++ media.js
@@ -0,0 +1,10 @@
+let m1 = (8 + 9 + 7) / 3
+let m2 = (4 + 5 + 6) / 3
+
+let somaM = m1 + m2
+let mediaF = somaM / 2
+
+console.log(`A media de 8, 9 e 7 é = ${m1}`)
+console.log(`A media de 4, 5 e 6 é = ${m2}`)
+console.log(`A soma das médias é = ${somaM}`)
+console.log(`A média das médias é = ${mediaF}`)
